mwalle/osccap

commit d8fadeef577cf7ee8ef0fa5d0cb7bb952ec43316

oscilloscope: fix issue with failed idn query
Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

osccap/oscilloscope/__init__.py
@@ -32,7 +32,10 @@ class Oscilloscope(object):
 
     def _update_manufacturer_model(self):
         """For legacy purpose we update the type."""
-        (self._manufacturer, self._model) = self.get_idn()[0:2]
+        try:
+            idn = self.get_idn()[0:2]
+        except Exception as e:
+            pass
 
     def get_manufacturer(self):
         return self._manufacturer

commit 714fddcfd43a5ccb612f5bfd2af0de1edf26dabe

oscilloscope: manufacturer and model are not stored
Fixes: d8fadeef577cf7e

Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
Signed-off-by: Jochen Baltes <jochen.baltes@gmail.com>

osccap/oscilloscope/__init__.py
@@ -33,7 +33,7 @@ class Oscilloscope(object):
     def _update_manufacturer_model(self):
         """For legacy purpose we update the type."""
         try:
-            idn = self.get_idn()[0:2]
+            (self._manufacturer, self._model) = self.get_idn()[0:2]
         except Exception as e:
             pass
 